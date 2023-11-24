5863c0a5e20f8cdc1cc28760981f4603418ca359
Matt Franchi <mattwfranc@gmail.com>
2023-11-24 14:41:41 -0700

    script to fetch num of results for a term

---
+++ scripts/fetch_num_results.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Dependencies: qhtml
+
+user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+
+# term is $1 surrounded by double quotes for exact match
+term="\"$1\""
+
+# Credit: https://stackoverflow.com/questions/53177265/extract-the-number-of-results-from-google-search, Pablo Bianchi
+curl --fail --silent --show-error -A "$user_agent" "https://www.google.com/search?hl=en&q=$term"
+curl  --fail --silent --show-error -A "$user_agent" "https://www.google.com/search?hl=en&q=$term"| htmlq "#result-stats" | grep -o "About.*results" | grep -o '[0-9]' | tr -d "\n"
+
