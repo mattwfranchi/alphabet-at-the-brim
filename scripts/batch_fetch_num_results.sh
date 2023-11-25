48e345cb9077e49010b58eeb5724149f7a6e65cd
Matt Franchi <mattwfranc@gmail.com>
2023-11-25 14:01:34 -0700

    batch fetch script updates

--- scripts/batch_fetch_num_results.sh
+++ scripts/batch_fetch_num_results.sh
@@ -13,12 +13,39 @@ if [ $# -eq 1 ]; then
     exit 1
 fi
 
+# A temporary abode for our fleet-footed processes
+temp_dir=$(mktemp -d)
+
+counter=0
+requests=0
+
 for line in $(cat $1); do
+    # Increment our counters
+    ((counter++))
+    ((requests++))
+
     # term is $1 surrounded by double quotes for exact match
     # generate csv format with term, num_results
     term="\"$line\""
-    num_results=$(sh fetch_num_results.sh $line)
-    echo "$term,$num_results" >> $2
-    # sleep 1 second to avoid Google's bot detection
-    sleep 1
+    (
+        num_results=$(sh fetch_num_results.sh $line)
+        echo "$term,$num_results" >> "$temp_dir/$line.csv"
+    ) &
+
+    # When our steps reach a hundred, we pause to let a second pass
+    if [ $requests -eq 100 ]; then
+        wait
+        sleep 1
+        requests=0
+    fi
 done
+
+wait # Wait for any remaining background tasks to complete their journey
+
+# Collate the results into the final scroll
+for file in "$temp_dir"/*; do
+    cat "$file" >> $2
+done
+
+# Clean up the remnants of our temporary encampment
+rm -rf "$temp_dir"
